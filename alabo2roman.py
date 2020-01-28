a014641324cfc3386096893704f958fde7dec9ad
YANYU ZHANG <55149684+zhangyanyu0722@users.noreply.github.com>
2020-01-27 20:35:24 -0500

    Add files via upload

---
+++ alabo2roman.py
@@ -0,0 +1,21 @@
+#!usr/bin/env python
+#Copyright @2020 Yanyu Zhang zhangya@bu.edu
+
+def alabo2roman(one_num):
+  if not isinstance(one_num,int) and one_num < 1:
+    return "ERROR"
+
+  num_list=[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  str_list=["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+  res=''
+  for i in range(len(num_list)):
+    while one_num>=num_list[i]:
+      one_num-=num_list[i]
+      res+=str_list[i]
+  return res
+
+if __name__ == '__main__':
+  print('The results of conversion are showing :')
+  one_num_list=[77,66,55,8,1200,34,65,3,21,99]
+  for one_num in one_num_list:
+    print(one_num,'----->',alabo2roman(one_num))
---
+++ alabo2roman_test.py
@@ -0,0 +1,28 @@
+#!usr/bin/env python
+#Copyright @2020 Yanyu Zhang zhangya@bu.edu
+
+from alabo2roman import alabo2roman
+
+def test_right(): 
+	assert alabo2roman(1213) == "MCCXIII"
+	assert alabo2roman(20) == "XX"
+	assert alabo2roman(384) == "CCCLXXXIV"
+	assert alabo2roman(99) == "XCIX"
+	assert alabo2roman(999) == "CMXCIX"
+	assert alabo2roman(3000) == "MMM"
+	assert alabo2roman(100) == "C"
+    assert alabo2roman(3999) == "MMMCMXCIX"
+    assert alabo2roman(12) == "XII"
+    assert alabo2roman(432) == "CDXXXII"
+
+def test_error():
+	assert alabo2roman("99.9") == "ERROR"
+	assert alabo2roman(-1) == "ERROR"
+	assert alabo2roman("String") == "ERROR"
+	assert alabo2roman(23.4) == "ERROR" 
+	assert alabo2roman(-3.4) == "ERROR"
+	assert alabo2roman(0.0) == "ERROR"
+	assert alabo2roman("-123") == "ERROR"
+	assert alabo2roman(-12345) == "ERROR"
+	assert alabo2roman("osama") == "ERROR"
+	assert alabo2roman(123.5) == "ERROR"
