a0503965fa6145338ffde66697a9e8f2d3170a3b
YANYU ZHANG <55149684+zhangyanyu0722@users.noreply.github.com>
2020-01-27 21:06:28 -0500

    Update alabo2roman_test.py

--- alabo2roman_test.py
+++ alabo2roman_test.py
@@ -11,8 +11,8 @@ def test_right():
 	assert alabo2roman(3000) == "MMM"
 	assert alabo2roman(100) == "C"
 	assert alabo2roman(3999) == "MMMCMXCIX"
-    	assert alabo2roman(12) == "XII"
-    	assert alabo2roman(432) == "CDXXXII"
+	assert alabo2roman(12) == "XII"
+	assert alabo2roman(432) == "CDXXXII"
 
 def test_error():
 	assert alabo2roman("99.9") == "ERROR"
